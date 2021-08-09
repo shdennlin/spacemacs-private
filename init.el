ec3336aefa02eb0ad930c982badf074d9401f893
shdennlin <shawndennislin@gmail.com>
2021-08-09 23:12:14 +0800

    [fix] remove "fzf" layers

--- init.el
+++ init.el
@@ -133,7 +133,6 @@ values."
      ;; =========== Web services ===========
      search-engine
      ;; =========== Custom Layer ===========
-     fzf
      shdennlin
      )
    ;; List of additional packages that will be installed without being
